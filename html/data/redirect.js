b495d8191bd3a2c4b79face35b0ddfb169a6344c
Alvina LE SAINT <ale-sain@paul-f4Ar4s4.clusters.42paris.fr>
2023-09-19 15:41:14 +0200

    mini webserv, working well except the BIG ENORMOUS MTF BUG

--- html/data/redirect.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("redirectForm").addEventListener("submit", function(event)
-{
-    event.preventDefault();
-    var url = document.getElementById("urlInput").value;
-    if (url !== "")
-      window.location.href = url;
-});
-  
